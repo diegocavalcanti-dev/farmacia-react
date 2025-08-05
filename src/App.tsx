052decbc17a872f4ac0fa6edab4ec1c5171f44ee
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-05 14:17:51 -0300

    fix: corrigindo home

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <div className="min-h-[80vh]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Home />
       </div>
       <Footer />
-    </BrowserRouter>
+    </>
   )
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,21 @@
 // src/components/navbar/Navbar.tsx
 
-import { Link } from "react-router-dom"
-
 function Navbar() {
   return (
     <div className='w-full bg-blue-600 text-white flex justify-center py-4'>
       <div className="container flex justify-between items-center text-lg">
-        <Link to='/' className="logo">iFarma Tech</Link>
+        <a href="#" className="logo">iFarma Tech</a>
 
         <nav>
           <ul className="navbar-menu">
             <li className="navbar-item">
-              <Link to="/produtos">Produtos</Link>
+              <a href="#">Produtos</a>
             </li>
             <li className="navbar-item">
-              <Link to="/categorias">Categorias</Link>
+              <a href="#">Categorias</a>
             </li>
             <li className="navbar-item">
-              <Link to="/cadastrarcategoria">Cadastrar Categoria</Link>
+              <a href="#">Cadastrar Categoria</a>
             </li>
           </ul>
         </nav>
